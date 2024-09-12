6fe36d9194e3b30574d9b0e6f10c3fa3f23c67ff
AAIDANN <114506075+AAIDANN@users.noreply.github.com>
2024-09-12 12:49:36 +0000

    work for 9/12

--- main.js
+++ main.js
@@ -37,6 +37,30 @@ if(userAnswer) {
     }
   }
 } else {
+  ti.output("Does your logo or name contain an animal????");
+  userAnswer = await ti.readYesOrNo();
+  if (userAnswer) {
+    ti.output("Does your team have blue in their team color??? ");
+    userAnswer = await ti.readYesOrNo();
+    if (userAnswer) {
+      ti.output("Is your team located East of the Mississippi River??");
+      userAnswer = await ti.readYesOrNo();
+      if (userAnswer) {
+        ti.output("Is your team trash this year???");
+        userAnswer = await ti.readYesOrNo();
+        if (userAnswer) {
+          ti.output("Your team is the Carolina Panthers");
+          ti.output("Happy Driving!!");
+        } else {
+          ti.output("your team is the Detroit Lions!!!!!!")
+        }
+      } else {
+        ti.output("Has your team lost to the New England Patriots twice in the superbowl")
+        
+      }
+    }
+  }
+
   
 }
 
